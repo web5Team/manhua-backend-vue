1aa45377c319c8204461c7f88ac07b0b2195ee0d
jim <jim.zh@google.com>
2025-05-08 16:24:18 +0800

    add start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "Installing dependencies..."
 
 # 使用pm2启动项目
 echo "Starting application with PM2..."
-pm2 start ecosystem.config.js --env production
+pm2 start ecosystem.config.js --env development
 
 # 保存pm2进程列表
 pm2 save
